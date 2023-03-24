02b8cc921957f940c2992fdf16b2ce8f0feb4875
Fernandojunqueira <fernandojunqueira12@gmail.com>
2023-03-24 18:32:49 -0300

    feat: docs

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,14 @@ import "express-async-errors"
 import express, {Application} from "express"
 import { globalRoutes } from "./routers"
 import handleError from "./errors/handleError"
+import swaggerUi from "swagger-ui-express"
+import swaggerDocs from "./swagger.json"
 
 const app:Application = express()
 
 app.use(express.json())
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.use(globalRoutes)
 app.use(handleError)
