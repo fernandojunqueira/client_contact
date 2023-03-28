52d4057c4ebe785abb0b73a9ca5655b3c802e50e
Fernandojunqueira <fernandojunqueira12@gmail.com>
2023-03-28 08:52:08 -0300

    feat:add cors config

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,13 @@ import { globalRoutes } from "./routers"
 import handleError from "./errors/handleError"
 import swaggerUi from "swagger-ui-express"
 import swaggerDocs from "./swagger.json"
+import cors from "cors"
 
 const app:Application = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
